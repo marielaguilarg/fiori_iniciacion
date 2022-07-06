2b54995f8cef2c0dc77ce26b93a7193d7a0241bf
mariel-aguilar <mariel@hcdsoluciones.com>
2022-07-06 00:03:12 +0000

    first commit on development

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["hcdsoluciones/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"hcdsoluciones/invoices/test/unit/controller/Mainview.controller"
+], function () {
+	"use strict";
+});
